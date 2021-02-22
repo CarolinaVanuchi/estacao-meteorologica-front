f2fe1706899f7259a748e86df86f498993484d0d
Maria Carolina Silva Vanuchi <carolina.vanuchi@srssistemas.com>
2021-02-22 15:12:19 -0300

    geração de grafico

---
+++ view/pages/gera_grafico_temp.php
@@ -0,0 +1,40 @@
+<?php
+	// $url = "http://192.168.0.150:5505/api/today";
+	$url = "http://9377dcd59f73.ngrok.io/api/historic?date1=2020-01-10&date2=2021-02-20";
+	$json_str = json_decode(file_get_contents($url));
+
+	$output_file = '../../assets/imgs/temperatura.png';
+	$horas = array();
+	$valores = array();
+	$i = 0;
+	foreach($json_str->data as $e) {
+		$horas[$i]      = $e->req_time;
+		$valores[$i]    = $e->temp;
+		$i = $i + 1;
+	} 	
+	$horas = array();
+	$valores = array();
+	$data = array();
+	$i = 0;
+	$y = 0;
+	$n_points = count($json_str->data);	
+	foreach($json_str->data as $e) {
+		$date_time = $e->req_date . ' ' . $e->req_time;
+		$valor	   = $e->temp;
+		array_push($data, array('', strtotime($date_time), $valor));
+	}
+	require('../../lib/phplot-6.2.0/phplot.php');
+    $plot = new PHPlot();
+    $plot->SetImageBorderType('plain');
+    $plot->SetPlotType('lines');
+    $plot->SetDataType('data-data');
+	$plot->SetXLabelType('time', '%Y-%m-%d %H:%M:%S');
+	$plot->SetXDataLabelType('');
+	$plot->SetXLabelAngle(90);
+    $plot->SetDataValues($data);
+    $plot->SetTitle('Temperatura');
+    $plot->SetPlotAreaWorld(NULL, 0, NULL, NULL);
+	$plot->SetIsInline(1);
+	$plot->SetOutputFile($output_file);
+    $plot->DrawGraph();
+?>
--- view/pages/index.php
+++ view/pages/index.php
@@ -1,8 +1,9 @@
-  <div id="header"> <?php include("../layout/header.html"); ?> </div> 
-  <?php
-    $url = "http://192.168.0.150:5505/api/today";
-    $json_str = json_decode(file_get_contents($url));
-  ?>
+<?php
+  // $url = "http://192.168.0.150:5505/api/today";
+  $url = "http://9377dcd59f73.ngrok.io/api/historic?date1=2020-01-01&date2=2021-02-20";
+  $json_str = json_decode(file_get_contents($url));
+?>
+<div id="header"> <?php include("../layout/header.html");?> </div> 
 
     <main role="main" class="container-fluid">
       <div class="card text-white bg-info mb-12">  
@@ -19,74 +20,68 @@
           <h5 class="card-title " style="margin-top: 35; ">Temperatura (ºC)</h5>
           <div class="row">
          
-            <div class="col col-lg-4 col-md-4 col-sm-4">
+            <div class="col col-lg-6 col-md-6 col-sm-6">
               <div id="table"> <?php include("../../config/index/temperatura.php"); ?></div> 
             </div>
             
-            <div class="col col-lg-8 col-md-8 col-sm-8">
-              <?php include("../../config/index/grafico_temperatura.php"); ?>
-              <div id="chart_temp"></div>
+            <div class="col col-lg-6 col-md-6 col-sm-6">
+              <img src="../../assets/imgs/temperatura.png" >
             </div>
           </div>
 
           <h5 class="card-title" style="margin-top: 35; ">Incidência Solar (W/m<sup>2</sup>)</h5>
           <div class="row">
           
-            <div class="col col-lg-4 col-md-4 col-sm-4">
+            <div class="col col-lg-6 col-md-6 col-sm-6">
               <div id="table"> <?php include("../../config/index/incidency_sun.php"); ?></div> 
             </div>
             
-            <div class="col col-lg-8 col-md-8 col-sm-8">
-              <?php include("../../config/index/grafico_incidency_sun.php"); ?>
-              <div id="chart_chuva"></div>
+            <div class="col col-lg-6 col-md-6 col-sm-6">
+              <img src="../../assets/imgs/incidency_sun.png" >
             </div>
           </div>
 
           <h5 class="card-title" style="margin-top: 35; ">Humidade (%) </h5>
           <div class="row">
-            <div class="col col-lg-4 col-md-4 col-sm-4">
+            <div class="col col-lg-6 col-md-6 col-sm-6">
               <div id="table"> <?php include("../../config/index/humidity.php"); ?></div> 
             </div>
             
-            <div class="col col-lg-8 col-md-8 col-sm-8">
-              <?php include("../../config/index/grafico_humidity.php"); ?>
-              <div id="chart_solar"></div>
+            <div class="col col-lg-6 col-md-6 col-sm-6">
+              <img src="../../assets/imgs/humidity.png" >
             </div>
           </div>
 
           <h5 class="card-title" style="margin-top: 35; ">Precipitação (mm)</h5>
           <div class="row">
-            <div class="col col-lg-4 col-md-4 col-sm-4">
+            <div class="col col-lg-6 col-md-6 col-sm-6">
               <div id="table"> <?php include("../../config/index/precipitation.php"); ?></div> 
             </div>
             
-            <div class="col col-lg-8 col-md-8 col-sm-8">
-              <?php include("../../config/index/grafico_precipitation.php"); ?>
-              <div id="chart_precipitation"></div>
+            <div class="col col-lg-6 col-md-6 col-sm-6">
+              <img src="../../assets/imgs/precipitation.png" >
             </div>
           </div>
 
           <h5 class="card-title" style="margin-top: 35; ">Ponto de Orvalho (ºC)</h5>
           <div class="row">
-            <div class="col col-lg-4 col-md-4 col-sm-4">
+            <div class="col col-lg-6 col-md-6 col-sm-6">
               <div id="table"> <?php include("../../config/index/dew_point.php"); ?></div> 
             </div>
             
-            <div class="col col-lg-8 col-md-8 col-sm-8">
-              <?php include("../../config/index/grafico_dew_point.php"); ?>
-              <div id="chart_dew_point"></div>
+            <div class="col col-lg-6 col-md-6 col-sm-6">
+              <img src="../../assets/imgs/dew_point.png" >
             </div>
           </div>
 
           <h5 class="card-title" style="margin-top: 35; ">Sensação Térmica (ºC)</h5>
           <div class="row">
-            <div class="col col-lg-4 col-md-4 col-sm-4">
+            <div class="col col-lg-6 col-md-6 col-sm-46">
               <div id="table"> <?php include("../../config/index/heat_index.php"); ?></div> 
             </div>
             
-            <div class="col col-lg-8 col-md-8 col-sm-8">
-              <?php include("../../config/index/grafico_heat_index.php"); ?>
-              <div id="chart_heat_index"></div>
+            <div class="col col-lg-6 col-md-6 col-sm-6">
+              <img src="../../assets/imgs/heat_index.png" >
             </div>
           </div>
 
