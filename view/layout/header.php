cbd0b0874e8de4a4790b9bdefabecc1380499381
Maria Carolina Silva Vanuchi <carolina.vanuchi@srssistemas.com>
2021-03-02 22:32:20 -0300

    retirada importacao do google charts

--- view/layout/header.php
+++ view/layout/header.php
@@ -8,7 +8,6 @@
 
   <link href="../../assets/css/bootstrap.min.css" rel="stylesheet">
   <link href="../../assets/css/navbar-top-fixed.css" rel="stylesheet">
-  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
    <?php 
     
